98c339411a94f70dda3f6b6454f7f2dacbd3bd6f
KPVBY <kpvbygm@gmail.com>
2020-07-08 19:39:27 +0300

    adaptation

---
+++ script/index.js
@@ -0,0 +1,32 @@
+const showMap = () => {
+    let form = document.getElementById('map');
+    if (form.style.display == 'none'||form.style.display == '') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+};
+const hiddenMap = () => {
+    let form = document.getElementById('map');
+    if (form.style.display = 'block') {
+        form.style.display = 'none';
+    } else {
+        form.style.display = 'none';
+    }
+};
+const showHelper = () => {
+    let form = document.getElementById('helper');
+    if (form.style.display == 'none'||form.style.display == ''){
+        form.style.display = 'flex';
+    } else {
+        form.style.display = 'none';
+    }
+};
+const hideHelper = () => {
+    let form = document.getElementById('helper');
+    if (form.style.display = 'flex'){
+        form.style.display = 'none';
+    } else {
+        form.style.display = 'none';
+    }
+};
